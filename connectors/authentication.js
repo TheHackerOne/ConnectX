a67e816fa47cb3c8ede94ea7292ab43299510c21
TheHackerOne <yash2200@gmail.com>
2020-02-21 12:11:46 +0530

    added login and signup controller

--- connectors/authentication.js
+++ connectors/authentication.js
@@ -17,6 +17,7 @@ exports.postSignUp = (req, res, next) => {
     .then(user => {
         if(user){
             return res.render('auth/signup',{
+                pageTitle: 'signup',
                 error: true
             })
         }
@@ -49,22 +50,21 @@ exports.postLogIn = (req, res, next) => {
     const user_email = req.body.email;
     const user_password = req.body.password;
 
-    user
-        .findOne({ user_email: user_email, user_password: user_password })
+    User
+        .findOne({ email: user_email, password: user_password })
         .then(user => {
         if (user) {
-            // console.log(user._id.toString());
             req.session.isloggedin = true;
-            const url = `/credentials/${user._id}`;
-            console.log(url);
-            res.redirect(url);
-        } else {
-            res.render("signup", {
-            pageTitle: "Signin",
-            text: "NO USER FOUND!! TRY SIGNUP ->",
-            error: false
-            });
+            req.session.user = user;
+            // const url = `/credentials/${user._id}`;
+            // console.log(url);
+            return res.redirect('/credentials');
         }
+        res.render("/login", {
+            pageTitle: "login",
+            error: true
+        });
+        
         })
         .catch(err => {
         console.log(err);
